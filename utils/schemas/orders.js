a8861b3ab3ce0e30f58fda338587300f1fe6e65d
pedro412 <pedro.alvarez412@outlook.com>
2019-10-23 14:16:15 -0500

    added order schemas

---
+++ utils/schemas/orders.js
@@ -0,0 +1,20 @@
+const joi = require('@hapi/joi');
+const { userIdSchema } = require('./users');
+const { productIdSchema } = require('./Products');
+
+const orderIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
+const orderItemsSchema = joi.array(productIdSchema).min(1);
+const orderDateSchema = joi.date().default(Date.now());
+const orderTotalSchema = joi.number();
+
+const createOrderSchema = {
+  items: orderItemsSchema.required(),
+  date: orderDateSchema.required(),
+  orderTotalSchema: orderTotalSchema.required(),
+  userId: userIdSchema
+};
+
+module.exports = {
+  orderIdSchema,
+  createOrderSchema
+};
